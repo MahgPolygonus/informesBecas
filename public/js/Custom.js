6bccb9b022500360dc8b107543bd337b66583f6e
Esteban Henao <esteban.c@polygon.us>
2020-10-09 17:25:35 -0500

    Vista principal y archivo custom.js

---
+++ public/js/Custom.js
@@ -0,0 +1,7 @@
+$( document ).ready(function() {
+    $(".buttons-copy").html('Copiar').css({"background-color":"#00adff","color":"white"});
+	$(".buttons-csv").css({"background-color":"#6fd42a","color":"white"});
+	$(".buttons-pdf").css({"background-color":"#ff3838","color":"white"});
+	$(".buttons-print").html('Imprimir').css({"background-color":"gray","color":"white"});
+	$(".buttons-excel").css({"background-color":"#389638","color":"white"});
+});
