b8519e8c2b91d14cedc5fe5eba7a8252de54719e
Miguel Holguin <Miguel.h@polygon.us>
2020-10-13 16:57:09 -0500

    Correcciones

--- app/Views/Reportsview.php
+++ app/Views/Reportsview.php
@@ -140,7 +140,7 @@
                       <tbody>
                         <tr>
                           <td>Tiger Nixon</td>
-                          <td>System Architect</td>
+                          <td>System Architesssssssssssssssssssssssct</td>
                           <td>Edinburgh</td>
                           <td>61</td>
                           <td>2011/04/25</td>
